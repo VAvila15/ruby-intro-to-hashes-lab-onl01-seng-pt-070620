d420761b106a29e4a2e60617a20d6d4b8e089db5
Victor Avila <avilavictor1570@gmail.com>
2020-07-18 20:11:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,12 +11,12 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {railroads: {pieces: 4}}
+	monopoly = {railroads: {pieces: 4, name: {}}}
 
 end
 
 def monopoly_with_third_tier
-	monopoly = {railroads: {pieces: 4, name: {}}}
+	monopoly = {railroads: {pieces: 4}}
 
 end
 
